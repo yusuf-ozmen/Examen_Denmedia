bb0ff652dfa99e09750baca758c0bc546efc46eb
yusuf-ozmen <103484064+yusuf-ozmen@users.noreply.github.com>
2022-04-20 16:08:35 +0200

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<style>
+
+</style>
+</head>
+<body>
+<?php
+include_once('header.php');
+?>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col p-1 bg-dark text-white"></div>
+    <div class="col p-1 bg-dark text-white"></div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-6">
+	<table class="table table-bordered">
+		<tr>
+			<th class="bg-primary text-white">Naam gerecht</th>
+			<th class="bg-primary text-white">Prijs</th>
+			<th class="bg-primary text-white">Beschrijving</th>
+		</tr>
+		<tr>
+			<td>TZATZIKI</td>
+			<td>€ 3</td>
+			<td>fresh dip with yogurt, garlic and cucumber</td>
+		</tr>
+		<tr>
+			<td>ELLINIKI SALATA</td>
+			<td>€ 5.50</td>
+			<td>Greek salad with olives and feta cheese</td>
+		</tr>
+		<tr>
+			<td>INKTVISRINGEN</td>
+			<td>€ 6.50</td>
+			<td>squid rings with our own garlic sauce</td>
+		</tr>
+		<tr>
+			<td>COURGETTEBALLETJES</td>
+			<td>€ 6.50</td>
+			<td>homemade zucchini balls with feta</td>
+		</tr>
+		<tr>
+			<td>PITA KIP</td>
+			<td>€ 5.50</td>
+			<td>roasted pita bread with chicken breast and garlic sauce</td>
+		</tr>
+		<tr>
+			<td>PITA BIFTEKI</td>
+			<td>€ 7</td>
+			<td>toasted pita bread with minced beef and tzatziki</td>
+		</tr>
+		<tr>
+			<td>PITABROOD</td>
+			<td>€ 1.50</td>
+			<td>loose per piece</td>
+		</tr>
+	</table>
+  </div>
+  <div class="col">
+	<table class="table table-bordered">
+		<tr>
+			<th class="bg-primary text-white">Naam gerecht</th>
+			<th class="bg-primary text-white">Prijs</th>
+			<th class="bg-primary text-white">Beschrijving</th>
+		</tr>
+		<tr>
+			<td>BIJ DE FRIETJES</td>
+			<td>€ 0.50</td>
+			<td>extra mayonnaise and ketchup</td>
+		</tr>
+		<tr>
+			<td>PITA VEGETAR</td>
+			<td>€ 5.50</td>
+			<td>toasted pita bread with feta, tomatoes and onion</td>
+		</tr>
+		<tr>
+			<td>PITA HALLOUMI</td>
+			<td>€ 6</td>
+			<td>toasted pita bread with grilled cypriot goat cheese halloumi</td>
+		</tr>
+		<tr>
+			<td>DOLMADES</td>
+			<td>€ 6.50</td>
+			<td>vine leaves stuffed with risotto</td>
+		</tr>
+		<tr>
+			<td>PITA SOUVLAKI</td>
+			<td>€ 6.50</td>
+			<td>toasted pita bread with grilled pork tenderloin skewer and tzatziki</td>
+		</tr>
+		<tr>
+			<td>PITA GYROS</td>
+			<td>€ 7</td>
+			<td>toasted pita bread with vegan gyros, mushrooms, tomatoes, onion and fries</td>
+		</tr>
+		<tr>
+			<td>RODOS PITA</td>
+			<td>€ 7.50</td>
+			<td>toasted pita bread with gyros of chicken pieces and tzatziki</td>
+		</tr>
+	</table>
+  </div>
+</div>
+<br /><br /><br /><br />
+<br /><br /><br /><br />
+<?php
+include_once('footer.php');
+?>
+</body>
+</html>
