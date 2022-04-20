414321e32dd05338705d6d3486d358379eb89063
yusuf-ozmen <103484064+yusuf-ozmen@users.noreply.github.com>
2022-04-20 16:12:26 +0200

    Add files via upload

---
+++ admin/footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+ 
+<html>
+ 
+<head>
+    <title>Simple web Development Template</title>
+	<link rel="stylesheet" href="css/footerstyle.css" type="text/css">
+</head>
+ 
+<body>
+    <footer class="background" style="background: #0B0B45">
+        <p class="text-footer" style="text-align: center; color:white; padding-bottom: 30px; font-size: 20px">
+            Copyright ©-All rights are reserved<br /><br />
+			Maandag:	10.00-19.00<br />
+			Dinsdag:	11.00-20.00<br />
+			Woensdag:	11.00-20.00<br />
+			Donderdag:	10.00-19.00<br />
+			Vrijdag:	12.00-18.00<br />
+			Zaterdag:	Gesloten<br />
+			Zondag:		Gesloten<br />
+		</p>
+    </footer>
+</body>
+ 
+</html>
---
+++ admin/home.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+ 
+<html>
+ 
+<head>
+    <title>Homepage</title>
+	<link rel="stylesheet" href="css/homepagestyle.css" type="text/css">
+</head>
+
+<style>
+.bgbnr {
+  /* The image used */
+  background-image: url("image/homebanner.jpg");
+
+  /* Full height */
+  height: 100%; 
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
+
+<body>
+<?php
+include_once('header.php');
+?>
+
+</head>
+<body>
+	<div class="bgbnr"><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /></div>
+
+    <section class="firstsection">
+        <div class="box-main">
+            <div class="firstHalf">
+                <h1 class="text-big" id="web">Welcome to Rhodes Good Taste</h1><br />
+                <p class="text-small">
+                    Do you want to experience modern Greek cuisine at its best?
+					Then come to Rodos Good Taste, our atmospheric restaurant in the heart of Leiden.
+					You can drop by for a delicious coffee, a drink with a bite, an extensive dinner…
+					and basically everything in between! Our door is open for you every day (closed on Tuesdays).
+					So you can easily combine an evening at the cinema or a day of shopping with a visit to Rodos Good Taste.
+					Enjoy classic Greek hospitality along with the flavors of hip Athens and Mykonos today.
+					Not everyday, but exclusive. Tada!
+                </p>
+ 
+ 
+            </div>
+        </div>
+    </section>
+ 
+    <section class="secondsection">
+        <div class="box-main">
+            <div class="firstHalf">
+                <h1 class="text-big" id="program">
+                    Different from other Greek restaurants
+                </h1><br />
+                <p class="text-small">
+                    We serve dishes that you will not find on the menu at other Greek restaurants.
+					We have also put the classics, such as moussaka and souvlaki, in our own jacket.
+					The emphasis is on taste experience and quality.
+					Our Greek chefs have a great culinary knowledge and continue to expand it.
+					The menu is therefore regularly updated. 'Cause we don't settle for good
+					but always want better. In short,
+					with us not too large portions and a ceiling full of vines.
+					But original dishes of high quality, served in a modern establishment.
+                </p>
+ 
+ 
+            </div>
+        </div>
+    </section>
+ 
+<?php
+include_once('footer.php');
+?>
+</body>
+ 
+</html>
